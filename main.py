358567fdf9b061ce0c9078792eb2e062a5aa43f3
Anzywiz <anzywiz@gmail.com>
2025-02-27 19:07:22 +0100

    Update: Place config data in utils.py

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 import asyncio
 from utils import run_all
-import json
-
-
-try:
-    # Load data from the JSON file
-    with open('config.json', "r") as file:
-        data = json.load(file)
-except FileNotFoundError:
-    raise FileNotFoundError(f"config.json file does not exist. Create one")
-except json.JSONDecodeError:
-    raise ValueError(f"The config file is not a valid JSON file.")
+from utils import data
 
 private_keys = data.get("private_keys", [])
 
